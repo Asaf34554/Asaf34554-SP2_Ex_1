7e7e61a7685ff9495378fd67b3d74654f4e681c8
Asaf34554 <Asaf34554@gmail.com>
2023-04-13 19:38:56 +0300

    Final

--- sources/card.cpp
+++ sources/card.cpp
@@ -10,3 +10,14 @@ Card:: Card(){
     type = "None";
     name = "None";
 }
+void Card:: set_Card_Value(int value){
+    this->valueOfCard = value;
+}
+
+void Card:: set_Card_Type(string type){
+    this->type = type;
+}
+
+void Card:: set_Card_Name(string name){
+    this->name = name;
+}
--- sources/card.hpp
+++ sources/card.hpp
@@ -18,8 +18,8 @@ namespace ariel
         string get_Card_Type()const{return type;} // inline method
         int get_Card_Value()const{return valueOfCard;} // inline method
         string get_Card_Name()const{return name;} // inline method
-        void set_Card_Value(int value){valueOfCard = value;} // outline method
-        void set_Card_Type(string ctype){type = ctype;}; //outline method
-        void set_Card_Name(string cname){name=cname;} //outline method
+        void set_Card_Value(int value); // outline method
+        void set_Card_Type(string type); //outline method
+        void set_Card_Name(string name); //outline method
 };  
 };
